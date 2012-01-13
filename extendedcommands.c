d99a3ef2d3e7f642c70f26187108b58a9df2d076
Drew Walton <drewwalton19216801@gmail.com>
2012-01-13 01:09:26 -0700

    We don't need to wipe battery stats. EVER.

--- extendedcommands.c
+++ extendedcommands.c
@@ -882,6 +882,7 @@ void show_nandroid_menu()
     }
 }
 
+/* We don't need to wipe battery stats. EVER. Dianne Hackborn said so.
 void wipe_battery_stats()
 {
     ensure_path_mounted("/data");
@@ -889,6 +890,7 @@ void wipe_battery_stats()
     ensure_path_unmounted("/data");
     ui_print("Battery Stats wiped.\n");
 }
+*/
 
 void show_advanced_menu()
 {
@@ -899,7 +901,6 @@ void show_advanced_menu()
 
     static char* list[] = { "Reboot Recovery",
                             "Wipe Dalvik Cache",
-                            "Wipe Battery Stats",
                             "Report Error",
                             "Key Test",
                             "Show log",
@@ -941,15 +942,9 @@ void show_advanced_menu()
                 break;
             }
             case 2:
-            {
-                if (confirm_selection( "Confirm wipe?", "Yes - Wipe Battery Stats"))
-                    wipe_battery_stats();
-                break;
-            }
-            case 3:
                 handle_failure(1);
                 break;
-            case 4:
+            case 3:
             {
                 ui_print("Outputting key codes.\n");
                 ui_print("Go back to end debugging.\n");
@@ -964,12 +959,12 @@ void show_advanced_menu()
                 while (action != GO_BACK);
                 break;
             }
-            case 5:
+            case 4:
             {
                 ui_printlogtail(12);
                 break;
             }
-            case 6:
+            case 5:
             {
                 static char* ext_sizes[] = { "128M",
                                              "256M",
@@ -1012,7 +1007,7 @@ void show_advanced_menu()
                     ui_print("An error occured while partitioning your SD Card. Please see /tmp/recovery.log for more details.\n");
                 break;
             }
-            case 7:
+            case 6:
             {
                 ensure_path_mounted("/system");
                 ensure_path_mounted("/data");
@@ -1021,7 +1016,7 @@ void show_advanced_menu()
                 ui_print("Done!\n");
                 break;
             }
-            case 8:
+            case 7:
             {
                 static char* ext_sizes[] = { "128M",
                                              "256M",
